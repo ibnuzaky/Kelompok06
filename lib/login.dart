08e0733a2119f3ea59e7ce8b74abe39adb7a4f4b
bumiadiwijaya <bumiadiwijaya@gmail.com>
2022-09-06 08:18:33 +0700

    LoginKantin

--- lib/login.dart
+++ lib/login.dart
@@ -19,7 +19,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.fromARGB(255, 14, 231, 104),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.fromLTRB(24, 40, 24, 0),
